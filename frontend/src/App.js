aa5b2a78c3dffc67fba31c88bcbc11de92bdc8f9
Grinchek <warning686@gmail.com>
2025-06-13 18:18:47 +0300

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import CategoryList from './CategoryList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Категорії з Django API</h1>
+      <CategoryList />
+    </div>
+  );
+}
+
+export default App;
